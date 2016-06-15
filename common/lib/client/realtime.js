d9b9ccb82eada0d4eab5adb5cf1039494729c8a5
Simon Woolf <simon@ably.io>
2016-06-15 10:08:45 +0100

    Make channel.inProgress into a counter
    To allow concurrent locks, eg detaching a channel before presence sync
    has completed

--- common/lib/client/realtime.js
+++ common/lib/client/realtime.js
@@ -51,8 +51,11 @@ var Realtime = (function() {
 	/* called when a transport becomes connected; reattempt attach()
 	 * for channels that may have been inProgress from a previous transport */
 	Channels.prototype.onTransportActive = function() {
-		for(var channelId in this.inProgress)
-			this.inProgress[channelId].checkPendingState();
+		for(var channelId in this.inProgress) {
+			if(this.inProgress[channelId] > 0) {
+				this.all[channelId].checkPendingState();
+			}
+		}
 	};
 
 	Channels.prototype.setSuspended = function(err) {
@@ -82,21 +85,28 @@ var Realtime = (function() {
 
 	Channels.prototype.setInProgress = function(channel, inProgress) {
 		if(inProgress) {
-			this.inProgress[channel.name] = channel;
+			this.inProgress[channel.name] = this.inProgress[channel.name] || 0;
+			this.inProgress[channel.name]++;
 		} else {
-			delete this.inProgress[channel.name];
-			if(Utils.isEmpty(this.inProgress)) {
+			this.inProgress[channel.name]--;
+			if(this.hasNopending) {
 				this.emit('nopending');
 			}
 		}
 	};
 
 	Channels.prototype.onceNopending = function(listener) {
-		if(Utils.isEmpty(this.inProgress)) {
+		if(this.hasNopending()) {
 			listener();
 			return;
 		}
 		this.once('nopending', listener);
+	};
+
+	Channels.prototype.hasNopending = function() {
+		return Utils.arrEvery(Utils.valuesArray(this.inProgress, true), function(numLocks) {
+			return numLocks === 0;
+		});
 	};
 
 	return Realtime;
